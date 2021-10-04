506f3605da77fc811023598b4171706751a00f07
mohittly <mohittly@gmail.com>
2021-10-04 11:42:39 +0530

    Update acceptance_rate_by_date.sql

--- acceptance_rate_by_date.sql
+++ acceptance_rate_by_date.sql
@@ -1,19 +1,16 @@
 
-=========================
 
-Question :
+-- Question :
 
-What is the overall friend acceptance rate by date? 
-Your output should have the rate of acceptances by the date the request was sent. Order by the earliest date to latest.
+-- What is the overall friend acceptance rate by date? 
+-- Your output should have the rate of acceptances by the date the request was sent. Order by the earliest date to latest.
 
-Assume that each friend request starts by a user sending (i.e., user_id_sender) a friend request to another user (i.e., user_id_receiver) that's logged in 
-the table with action = 'sent'. 
+-- Assume that each friend request starts by a user sending (i.e., user_id_sender) a friend request to another user (i.e., user_id_receiver) that's logged in 
+-- the table with action = 'sent'. 
 
-If the request is accepted, the table logs action = 'accepted'. If the request is not accepted, no record of action = 'accepted' is logged.
+-- If the request is accepted, the table logs action = 'accepted'. If the request is not accepted, no record of action = 'accepted' is logged.
 
-=========================
-
-Answer :
+-- Answer :
 
 select 
   f1.date, 
@@ -28,6 +25,4 @@ where
 group by 
   f1.date
 ;
-
-=========================
 
